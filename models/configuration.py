7cae1ddba6d39a811d11d4be15a2830633fc6d68
Vincent Meijer <meijer.vincent@gmail.com>
2019-10-15 17:22:39 +0200

    #18 Cleanup default values in class

--- models/configuration.py
+++ models/configuration.py
@@ -1,7 +1,7 @@
 class Configuration:
     def __init__(self, db_name=None, db_user=None, db_password=None, db_host=None, db_port=None):
-        self.db_name = db_name if db_name is not None else "dycast"
-        self.db_user = db_user if db_user is not None else "postgres"
-        self.db_password = db_password if db_password is not None else "postgres"
-        self.db_host = db_host if db_user is not None else "localhost"
-        self.db_port = db_port if db_port is not None else "5432"
+        self.db_name = db_name or "dycast"
+        self.db_user = db_user or "postgres"
+        self.db_password = db_password or "postgres"
+        self.db_host = db_host or "localhost"
+        self.db_port = db_port or "5432"
