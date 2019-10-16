88db517f85a33b563147a5cbd572c6e96d9921fd
Vincent Meijer <meijer.vincent@gmail.com>
2019-10-16 17:00:20 +0200

    #18 Set base path for default config file location (otherwise stored in Documents folder on Windows). Group exceptions

--- services/configuration_service.py
+++ services/configuration_service.py
@@ -1,9 +1,18 @@
+import os
 import pickle
 from dycast_qgis.models.configuration import Configuration
 
 class ConfigurationService():
     def __init__(self, config_file_path=None):
-        self.config_file_path = config_file_path or "dycast_qgis.config"
+        self.config_file_path = config_file_path or self._get_default_config_path()
+
+    def _get_default_config_path(self):
+        return os.path.join(self._get_root_directory(), "dycast_qgis.config")
+
+    def _get_root_directory(self):
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        return os.path.abspath(
+            os.path.join(current_directory, ".."))
 
     def persist_config(self, config: Configuration):
         config_file = open(self.config_file_path, "wb")
@@ -13,7 +22,5 @@ class ConfigurationService():
     def load_config(self):
         try:
             return pickle.load(open(self.config_file_path, "rb"))
-        except EOFError:
-            return Configuration()
-        except TypeError:
+        except (FileNotFoundError, EOFError, TypeError):
             return Configuration()
