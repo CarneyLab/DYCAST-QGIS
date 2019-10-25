333a35798cd053b200875874bf4ef864806a194b
Vincent Meijer <meijer.vincent@gmail.com>
2019-10-25 16:41:57 +0200

    Do nullcheck before logging traceback

--- services/logging_service.py
+++ services/logging_service.py
@@ -9,10 +9,17 @@ MESSAGE_CATEGORY = 'Messages'
 def log_message(message, category: Qgis.MessageLevel = Qgis.Info):
     QgsMessageLog.logMessage(message, MESSAGE_CATEGORY, category)
 
+
 def log_exception(exception: Exception):
-    exc_type, exc_value, exc_traceback = sys.exc_info()
-    format_exception = traceback.format_exception(exc_type, exc_value, exc_traceback)
+    QgsMessageLog.logMessage(type(exception).__name__, MESSAGE_CATEGORY, Qgis.Critical)
     QgsMessageLog.logMessage(str(exception), MESSAGE_CATEGORY, Qgis.Critical)
-    QgsMessageLog.logMessage(repr(format_exception[0]), MESSAGE_CATEGORY, Qgis.Critical)
-    QgsMessageLog.logMessage(repr(format_exception[1]), MESSAGE_CATEGORY, Qgis.Critical)
-    QgsMessageLog.logMessage(repr(format_exception[2]), MESSAGE_CATEGORY, Qgis.Critical)
+    _log_formatted_traceback()
+
+
+def _log_formatted_traceback():
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    if exc_type:
+        format_exception = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        QgsMessageLog.logMessage(repr(format_exception[0]), MESSAGE_CATEGORY, Qgis.Critical)
+        QgsMessageLog.logMessage(repr(format_exception[1]), MESSAGE_CATEGORY, Qgis.Critical)
+        QgsMessageLog.logMessage(repr(format_exception[2]), MESSAGE_CATEGORY, Qgis.Critical)
