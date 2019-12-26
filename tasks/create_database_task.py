841a40bb4944d66db49bdd9767eb8f7e4753bb1c
Vincent Meijer <meijer.vincent@gmail.com>
2019-12-26 12:58:10 +0100

    #2 Only run setup_dycast command if db does not exist

--- tasks/create_database_task.py
+++ tasks/create_database_task.py
@@ -14,25 +14,30 @@ def get_current_directory():
     return os.path.dirname(os.path.realpath(__file__))
 
 
-def check_if_db_exists(config: Configuration):
+def check_if_db_exists(config: Configuration) -> bool:
     database_service = DatabaseService(config)
     return database_service.check_if_db_exists()
 
 
 def run(task, config: Configuration, force: bool):
-    import ptvsd
     log_message("Started create_database task", Qgis.Info)
-    with redirect_stdout():
-        from dycast_app.dycast import main as dycast_main
 
-        command = ["setup_dycast", "--monte-carlo-file",
-                   "Dengue_max_100_40000.csv"]
+    if (check_if_db_exists(config)):
+        log_message("Database already exists, skipping...", Qgis.Info)
+    else:
+        with redirect_stdout():
+            log_message("Database does not exists yet, setting up...", Qgis.Info)
 
-        if force:
-            command.append("--force-db-init")
+            from dycast_app.dycast import main as dycast_main
 
-        log_message(f"Running command: {command}", Qgis.Info)
-        dycast_main(command)
+            command = ["setup_dycast", "--monte-carlo-file",
+                    "Dengue_max_100_40000.csv"]
+
+            if force:
+                command.append("--force-db-init")
+
+            log_message(f"Running command: {command}", Qgis.Info)
+            dycast_main(command)
 
     return {"db_exists": check_if_db_exists(config)}
 
