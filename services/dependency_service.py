f50442127dd5928d2157f5f329f232ea5cfb3ab3
Vincent Meijer <meijer.vincent@gmail.com>
2020-06-01 14:33:39 +0200

    #15 Install dependencies only if not already installed

--- services/dependency_service.py
+++ services/dependency_service.py
@@ -1,8 +1,9 @@
 import os
 import subprocess
 from subprocess import Popen, PIPE
+from pathlib import Path
 from qgis.core import Qgis
-from dycast_qgis.services.logging_service import log_message
+from dycast_qgis.services.logging_service import log_message, log_exception
 
 class DependencyService():
 
@@ -10,23 +11,48 @@ class DependencyService():
         pass
 
     def install_dependencies(self):
-        log_message("Installing dependencies...", Qgis.Info)
+        if (self.installation_is_required()):
+            log_message("Installing dependencies...", Qgis.Info)
 
-        requirements_file = os.path.join(os.path.dirname(__file__), '..', 'requirements.txt' )
-        requirements_file_dycast = os.path.join(os.path.dirname(__file__), '..', 'dycast_app', 'init', 'requirements.txt' )
-        
-        upgrade_command = ['python', '-m', 'pip', 'install', '--upgrade', 'pip', '--user', '--no-warn-script-location']
-        installation_command = ['python', '-m', 'pip', 'install', '--requirement', requirements_file, '--user']
-        installation_command_dycast = ['python', '-m', 'pip', 'install', '--requirement', requirements_file_dycast, '--user']
-        
-        log_message("Upgrading PIP...", Qgis.Info)
-        self.run_subprocess(upgrade_command)
-        log_message("Installing QGIS plugin dependencies...", Qgis.Info)
-        self.run_subprocess(installation_command)
-        log_message("Installing Dycast app dependencies...", Qgis.Info)
-        self.run_subprocess(installation_command_dycast)
-        
-        log_message("Done installing dependencies.", Qgis.Info)
+            requirements_file = os.path.join(os.path.dirname(__file__), '..', 'requirements.txt' )
+            requirements_file_dycast = os.path.join(os.path.dirname(__file__), '..', 'dycast_app', 'init', 'requirements.txt' )
+            
+            upgrade_command = ['python', '-m', 'pip', 'install', '--upgrade', 'pip', '--user', '--no-warn-script-location']
+            installation_command = ['python', '-m', 'pip', 'install', '--requirement', requirements_file, '--user']
+            installation_command_dycast = ['python', '-m', 'pip', 'install', '--requirement', requirements_file_dycast, '--user']
+            
+            log_message("Upgrading PIP...", Qgis.Info)
+            self.run_subprocess(upgrade_command)
+            log_message("Installing QGIS plugin dependencies...", Qgis.Info)
+            self.run_subprocess(installation_command)
+            log_message("Installing Dycast app dependencies...", Qgis.Info)
+            self.run_subprocess(installation_command_dycast)
+            
+            log_message("Done installing dependencies.", Qgis.Info)
+
+    def installation_is_required(self):
+        log_message("Checking if dependencies are installed", Qgis.Info)
+        current_directory = Path(__file__).parent
+        task_directory = Path(os.path.join(current_directory, '..', 'tasks'))
+        if self.can_import_modules(current_directory) and self.can_import_modules(task_directory, 'dycast_qgis.tasks'):
+            log_message("Dependencies are installed", Qgis.Info)
+            return False
+        else:
+            return True
+
+    def can_import_modules(self, path: Path, module_prefix: str = ''):
+        try:
+            from importlib import import_module
+
+            for f in path.glob("*.py"):
+                if not f.stem.startswith("_"):
+                    import_module(f"{module_prefix}.{f.stem}", __package__)
+            
+            return True
+
+        except ImportError as e:
+            log_exception(e)
+            return True
 
     def run_subprocess(self, command):
         process = subprocess.Popen(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
