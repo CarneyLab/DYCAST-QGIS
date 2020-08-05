84a20ba329cce7c33669f18c9537e2d6241d5932
Vincent Meijer <meijer.vincent@gmail.com>
2020-08-05 16:54:14 +0200

    #6 Add parameters to generate_risk_task

--- tasks/generate_risk_task.py
+++ tasks/generate_risk_task.py
@@ -1,22 +1,42 @@
 import sys
 from qgis.core import Qgis
 
+from dycast_qgis.models.generate_risk_parameters import GenerateRiskParameters
 from dycast_qgis.services.logging_service import log_message, log_exception
 from dycast_qgis.util.redirect_stdout import redirect_stdout
 
 
-def run(task):
+def run(task, parameters: GenerateRiskParameters):
     log_message("Started generate_risk task", Qgis.Info)
-    
+
     if not hasattr(sys, 'argv'):
         sys.argv  = ['']
         
     from dycast_app.dycast import main as dycast_main
     
     with redirect_stdout():
-        command = ["generate_risk", "--monte-carlo-file", "Dengue_max_100_40000.csv"]
+        command = [
+            "generate_risk",
+            "--monte-carlo-file", "Dengue_max_100_40000.csv",
+            "--spatial-domain", parameters.spatialDomain,
+            "--temporal-domain", parameters.temporalDomain,
+            "--close-in-space", parameters.closeInSpace,
+            "--close-in-time", parameters.closeInTime,
+            "--case-threshold", parameters.caseThreshold,
+            "--startdate", parameters.startDate,
+            "--enddate", parameters.endDate,
+            "--srid-extent", parameters.sridOfExtent,
+            "--extent-min-x", parameters.extentMinX,
+            "--extent-min-y", parameters.extentMinY,
+            "--extent-max-x", parameters.extentMaxX,
+            "--extent-max-y", parameters.extentMaxY
+            ]
+
         log_message(f"Running command: {command}", Qgis.Info)
-        dycast_main(command)
+        try:
+            dycast_main(command)
+        except Exception as ex:
+            log_exception(ex)
 
 def finished(exception, result=None):
     if result:
