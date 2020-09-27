29f85f4ae5e9b589c2979862674efa24d9cce4ae
Vincent Meijer <meijer.vincent@gmail.com>
2020-09-27 15:12:25 +0200

    #6 Add task for installing dependencies

---
+++ tasks/install_dependencies_task.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import subprocess
+
+from qgis.core import Qgis, QgsApplication
+from qgis.PyQt.QtCore import Qt
+
+from dycast_qgis.services.dependency_service import DependencyService
+from dycast_qgis.services.logging_service import log_message, log_exception
+
+
+def run(task):
+    log_message("Started install_dependencies task", Qgis.Info)
+
+    dependency_service = DependencyService()
+    dependency_service.install_dependencies()
+
+
+def finished(exception, result=None):
+    if result:
+        log_message("Succesfully finished the install_dependencies task", Qgis.Success)
+    else:
+        log_message("Failed to run the install_dependencies task", Qgis.Warning)
+
+        if exception:
+            log_exception(exception)
+        else:
+            log_message("No exception was raised", Qgis.Warning)
